91aaf833306af7f3af9b50154a97bdd7d56afdf6
O'Neal, Stephen <stephen.oneal@viasat.com>
2016-01-24 11:06:12 -0600

    added language code support

--- pyccuweather/errors.py
+++ pyccuweather/errors.py
@@ -9,6 +9,13 @@ The Python Accuweather API
 http://www.github.com/chrisvoncsefalvay/pyccuweather/
 """
 
+class BadLanguageInput(BaseException):
+    """
+    Raised when the Language input is not correctly formed
+    """
+    def __str__(self):
+        return "Language selection must a 2 digit language code compliant with ISO 639-1"
+
 
 class MalformattedAPIKeyError(BaseException):
     """
